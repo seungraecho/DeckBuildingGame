7777bf125c4c8412f3f5ae8ed99a40356382d71c
seungraeCho <tmdfo6700@gmail.com>
2025-11-15 11:02:37 +0900

    merge completed

--- Assets/bhparkUnity/Script/GameRuleMaster.cs
+++ Assets/bhparkUnity/Script/GameRuleMaster.cs
@@ -1,11 +1,7 @@
 using UnityEngine;
 
-
-
 public class GameRuleMaster : MonoBehaviour
 {
-
-
     public GameObject DIceMachine;
 
     //1. 다이스 정보 가져오기
@@ -14,16 +10,12 @@ public class GameRuleMaster : MonoBehaviour
     //- 플레이어의 선택한 키워드
     //- 다이스 개수 X 선택 키워드 
 
-
-
     public GameObject ItemManager;
 
     //2. 아이템 정보 가져오기
 
     //- 아이템 키워드
     //- 아이템 효과
-
-
 
     public GameObject Monster;
 
@@ -34,10 +26,11 @@ public class GameRuleMaster : MonoBehaviour
 
     //=============
 
-
-
     // 데미지 산출
 
+
+
+    //=============
 
     DiceMachine diceMachine;
 
@@ -45,43 +38,23 @@ public class GameRuleMaster : MonoBehaviour
 
     Monster monster;
 
-
-
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
         diceMachine = DIceMachine.GetComponentInChildren<DiceMachine>();
-
         itemManager = ItemManager.GetComponentInChildren<ItemManager>();
-
         monster = Monster.GetComponentInChildren<Monster>();
 
-
-        diceMachine.RollDice();
+        Debug.Log("Game Rule Master is Running");
     }
 
     // Update is called once per frame
     void Update()
     {
-
-
-        Debug.Log("Game Rule Master is Running" );
-
-
         if(Input.GetKeyDown(KeyCode.Space))
         {
-
-
             diceMachine.RollDice();
-
-            Debug.Log("Dice Value: " + string.Join(",", diceMachine.diceValue));
-            
-
         }
-
-
     }
 }
 
