041a803c0ff8c9a8bf698d13460fe721e4baa138
parkbyeongho123-blip <parkbyeongho123@gmail.com>
2025-11-16 10:31:12 +0900

    feature-addDice6
    feature-addDice6

---
+++ DIceAnimation.cs
@@ -0,0 +1,81 @@
+using UnityEngine;
+
+public class DIceAnimation : MonoBehaviour
+{
+
+    Transform diceTransform;
+
+    private int _diceTransformRotationX;
+
+    private int _diceTransformRotationY;
+
+    private int _diceTransformRotationZ;
+
+
+    Vector3 _diceTransformPosition;
+
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+
+        diceTransform = GetComponent<Transform>();
+
+
+        _diceTransformPosition = diceTransform.position;
+
+
+
+        _diceTransformRotationX = 0;
+
+        _diceTransformRotationY = 0;
+
+        _diceTransformRotationZ = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        _diceTransformRotationX = _diceTransformRotationX + 1;
+
+        _diceTransformRotationY += 1;
+
+        _diceTransformRotationZ += 1;
+
+
+
+        _diceTransformPosition.x = _diceTransformRotationX;
+
+        _diceTransformPosition.y = _diceTransformRotationY;
+
+        _diceTransformPosition.z = _diceTransformRotationZ;
+
+
+        diceTransform.rotation = Quaternion.Euler(_diceTransformPosition);
+
+
+
+        OnMouseEnter();
+
+
+        
+    }
+
+
+
+    private void OnMouseEnter()
+    {
+        print("Mouse is On Dice.");
+    }
+
+
+
+    private void OnMouseDown()
+    {
+        print("Mouse click Dice");
+    }
+
+}
